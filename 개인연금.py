fe72af0676c7b75bf29f22580bad0a761bd8bcec
kkadin <kkadin@gmail.com>
2024-03-09 20:33:23 +0900

    개인연금.py - by changing mode, you can select pension or pension_irp

--- 개인연금.py
+++ 개인연금.py
@@ -8,15 +8,104 @@ Created on Sun Feb 25 09:44:47 2024
 import yfinance as yf
 import datetime
 
-# 추가 투자금
-money_add_won = 729778
+# IRP_개인연금 정보
+dic_pension_irp =  {
+                        # stock name 
+                        'list_stock_name'   : [ "kodex_bank",
+                                                "arirang_msci",
+                                                "tiger_SnP_500",
+                                                "kosef_nbond10y",
+                                                "kodex_sbond"],
+                        # standard stock number
+                        'list_stock_number' : ['091170.KS',
+                                               '195980.KS', 
+                                               '360750.KS', 
+                                               '148070.KS', 
+                                               '153130.KS'],
+                        # 목표 비율
+                        'list_portion'      : [ 5.0,
+                                               25.0,
+                                               30.0,
+                                               30.0,
+                                               10.0],
+                        # 보유 수량 입력
+                        'list_hold_num'     : [ 199,
+                                               747,
+                                               525,
+                                               69,
+                                               23 ],
+                        # 추가 투자금
+                        'money_add_won'     : 729778
+                        
+                   }
 
-# 보유 수량 입력
-list_hold_num = [ 199, 747, 525, 69, 23 ]
+# 연금저축 정보
+dic_pension =  {
+                        # stock name 
+                        'list_stock_name'   : [ "gold",
+                                                "kodex_usbond10y",
+                                                "kodex_bank",
+                                                "arirang_msci",
+                                                "tiger_SnP500",
+                                                "kosef_nbond10y",
+                                                "kodex_sbond"
+                                              ],
+                        # standard stock number
+                        'list_stock_number' : ['132030.KS',
+                                               '308620.KS',
+                                               '091170.KS',
+                                               '195980.KS',
+                                               '143850.KS',
+                                               '148070.KS',
+                                               '153130.KS'
+                                               ],
+                        # 목표 비율
+                        'list_portion'      : [ 5.0,
+                                               20.0,
+                                                1.0,
+                                               19.0,
+                                               25.0,
+                                               20.0,
+                                               10.0
+                                              ],
+                        # 보유 수량 입력
+                        'list_hold_num'     : [ 293,
+                                               1395,
+                                               120,
+                                               1708,
+                                               392,
+                                               138,
+                                               69],
+                        # 추가 투자금
+                        'money_add_won'     : 1153803
+                        
+                   }
 
+# 계산할 정보 선택
+PENSION = 0     # 연금 ( 연금저축 )
+PENSION_IRP = 1 # 연금IRP ( 개인연금 IRP )
+#mode = PENSION
+mode = PENSION_IRP
 
-# 목표 비율
-list_portion = [ 5.0, 25.0, 30.0, 30.0, 10.0]
+if mode == PENSION:
+    list_stock_name = dic_pension_irp['list_stock_name']
+    list_stock_number = dic_pension_irp['list_stock_number']
+    list_hold_num = dic_pension_irp['list_hold_num']
+    list_portion = dic_pension_irp['list_portion']
+    money_add_won = dic_pension_irp['money_add_won']
+elif mode == PENSION_IRP:
+    list_stock_name = dic_pension['list_stock_name']
+    list_stock_number = dic_pension['list_stock_number']
+    list_hold_num = dic_pension['list_hold_num']
+    list_portion = dic_pension['list_portion']
+    money_add_won = dic_pension['money_add_won']
+else:
+    print('invalid mode = {}'.format(mode))
+    list_stock_name = [];
+    list_stock_number = [];
+    list_hold_num = [];
+    list_portion = [];
+    money_add_won = [];
 
 # 오늘로 부터 날짜 이동 ( 며칠전 부터 ? )
 DAY_SHIFT = 5   
@@ -30,16 +119,7 @@ day_start = datetime.date.today() - datetime.timedelta(DAY_SHIFT)
 # 마치는 날짜
 day_end = day_start + datetime.timedelta(DAY_LENGTH)
 
-# 오늘 날짜
-# day_now = datetime.date.today()
-# day_end = day_now
-
-# 5일전 날짜
-# day_start = day_end - datetime.timedelta(5);
-# day_data_get = day_now - datetime.timedelta(2)
-
 # 날짜 사이 주가 추출
-list_stock_number = ['091170.KS','195980.KS', '360750.KS', '148070.KS', '153130.KS']
 list_stock_data = []
 for item in list_stock_number:
     stock_data = yf.download(item, start = day_start, end = day_end)  
@@ -74,14 +154,6 @@ for target, total, close_mean in zip ( list_target, list_total, list_close_mean)
     list_change_num.append(change_num)
 
 # 매매 수량 프린트
-list_stock_name =   [
-                        "kodex_bank",
-                        "arirang_msci",
-                        "tiger_SnP_500",
-                        "kosef_nbond10y",
-                        "kodex_sbond"
-                    ]
-
 print()
 print()
 for stock_name, change_num in zip(list_stock_name, list_change_num):
