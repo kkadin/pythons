4314e5c0693d0d5a9d83923de98cf52fe371d20e
kkadin <kkadin@gmail.com>
2025-03-16 13:37:25 +0900

    to do money_add_pension changes doesn't affect result

--- pensions/개인연금.py
+++ pensions/개인연금.py
@@ -8,9 +8,9 @@ from global_var import *
 import yfinance as yf
 import datetime
 
+list_hold_num = []              # 보유수량
 list_stock_name = []
-list_stock_number = []
-list_hold_num = []
+list_stock_number = []          # 주식 인식번호
 list_portion = []
 
 if mode == PENSION:
@@ -23,10 +23,10 @@ else:
     print('invalid mode = {}'.format(mode))
 
 for item in list_stock_info:
-    list_stock_name.append(item['name']) 
-    list_stock_number.append(item['number']) 
-    list_hold_num.append(item['hold_num']) 
-    list_portion.append(item['portion']) 
+    list_hold_num.append(item['hold_num'])
+    list_stock_number.append(item['number'])
+    list_stock_name.append(item['name'])
+    list_portion.append(item['portion'])
 
 # 시작 날짜
 day_start = datetime.date.today() - datetime.timedelta(DAY_SHIFT)
@@ -35,16 +35,21 @@ day_start = datetime.date.today() - datetime.timedelta(DAY_SHIFT)
 day_end = day_start + datetime.timedelta(DAY_LENGTH)
 
 # 날짜 사이 주가 추출
-list_stock_data = []
+# list_of_df_stock_data = []
+list_close_mean = []
 for item in list_stock_number:
-    stock_data = yf.download(item, start = day_start, end = day_end)  
-    list_stock_data.append(stock_data)
+    stock_data = yf.download(item, start = day_start, end = day_end)
+    # stock_data = yf.download(item, start="2025-01-01", end="2025-01-10")
+    # stock_data = yf.download("AAPL", start="2025-01-01", end="2025-01-10")
+    # list_of_df_stock_data.append(stock_data)
+    mean = stock_data["Close"].mean().iloc[0]
+    list_close_mean.append(mean)
 
 # 평균값 구하기
-list_close_mean = []
-for item in list_stock_data:
-    mean = item["Close"].mean()
-    list_close_mean.append(mean)
+# list_close_mean = []
+# for item in list_of_df_stock_data:
+#     mean = item["Close"].mean()
+#     list_close_mean.append(mean)
 
 # 총액 : 현재 평균값 * 보유수량 
 list_total = []
@@ -59,8 +64,8 @@ sum_of_total = sum(list_total)
 list_target = []
 for portion in list_portion:
     target = (sum_of_total + money_add_won) * portion/100
+    # print("target:{} = (sum_of_total:{} + money_add_won:{}) * portion:{} / 100".format(target, sum_of_total, money_add_won, portion))
     list_target.append(target)
-
 
 # 매매 수량
 list_change_num = []
@@ -72,7 +77,7 @@ for target, total, close_mean in zip ( list_target, list_total, list_close_mean)
 print()
 print()
 for stock_name, change_num in zip(list_stock_name, list_change_num):
+    # rounded_num = int(round(change_num.iloc[0], 0))
     rounded_num = int(round(change_num, 0))
-    # print(stock_name+" : {} " .format(rounded_num))
     print('{:>15}:{:>5}'.format(stock_name, rounded_num))
 
